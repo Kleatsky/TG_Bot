2b77ea40cf59bf9c2b0d675ace73d76779ff2653
salaga999999999 <salaga999999999@gmail.com>
2025-05-25 13:59:41 +0700

    Add subscribers

--- TG_Bot/ServerConsole/Program.cs
+++ TG_Bot/ServerConsole/Program.cs
@@ -1,10 +1,81 @@
-﻿namespace ServerConsole
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+
+namespace ServerConsole
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static string _filePath = @"Token.txt";
+        private static string _token;
+
+        private static void OnHandleUpdateStarted(object sender, string message)
         {
-            Console.WriteLine("Hello, World!");
+            if (sender == null) return;
+            Console.WriteLine($"Началась обработка сообщения '{message}'");
+        }
+        private static void OnHandleUpdateCompleted(object sender, string message)
+        {
+            if (sender == null) return;
+            Console.WriteLine($"Закончилась обработка сообщения '{message}'");
+        }
+        static async Task Main(string[] args)
+        {
+            if (File.Exists(_filePath))
+            {
+                try
+                {
+                    var lines = File.ReadAllLines(_filePath);
+                    _token = lines[0];
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+            }
+            else
+            {
+                return;
+            }
+
+            var cts = new CancellationTokenSource();
+            var bot = new TelegramBotClient(_token);
+            var me = await bot.GetMe();
+            Console.WriteLine($"Name: {me.FirstName} {me.LastName} | {me.Username} id: {me.Id} " +
+                $"Premial: {me.IsPremium} LanguageCode: {me.LanguageCode}");
+
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = Array.Empty<UpdateType>()
+            };
+
+            UpdateHandler updateHandler = new UpdateHandler();
+            updateHandler.OnHandleUpdateStarted += OnHandleUpdateStarted;//подписка с обработкой через OnHandleUpdateStarted
+            updateHandler.OnHandleUpdateCompleted += OnHandleUpdateCompleted;//подписка с обработкой через OnHandleUpdateCompleted
+
+            bot.StartReceiving(
+                updateHandler: updateHandler.HandleUpdateAsync,
+                errorHandler: updateHandler.HandleErrorAsync,
+                receiverOptions: receiverOptions,
+                cancellationToken: cts.Token
+            );
+
+
+
+
+            Console.ReadLine();
+            await cts.CancelAsync();
+
+            try
+            {
+                updateHandler.OnHandleUpdateStarted -= OnHandleUpdateStarted;//Отписка от события
+                updateHandler.OnHandleUpdateCompleted -= OnHandleUpdateCompleted;//Отписка от события
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error while unsubscribe: " + e.Message);
+            }
         }
     }
 }
---
+++ TG_Bot/ServerConsole/UpdateHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using static Telegram.Bot.TelegramBotClient;
+
+namespace ServerConsole
+{
+    internal class UpdateHandler : IUpdateHandler
+    {
+        public delegate void MessageHandler(object sender, string message);
+        public event MessageHandler OnHandleUpdateStarted;
+        public event MessageHandler OnHandleUpdateCompleted;
+        public async Task HandleUpdateAsync(ITelegramBotClient bot, Update update, CancellationToken cancellationToken)
+        {
+
+            if (update.Type == UpdateType.Message && update.Message != null)
+            {
+
+                var chatId = update.Message.Chat.Id;
+                var messageText = update.Message?.Text ?? "ERROR";
+
+                OnHandleUpdateStarted.Invoke(this, messageText);
+
+                await bot.SendMessage(
+                    chatId: chatId,
+                    text: "Сообщение успешно принято",
+                    cancellationToken: cancellationToken
+                );
+
+                OnHandleUpdateCompleted.Invoke(this, messageText);
+            }
+        }
+        public async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, HandleErrorSource source, CancellationToken cancellationToken)
+        {
+            Console.WriteLine(exception.Message);
+            await Task.Delay(2000, cancellationToken);
+        }
+    }
+}
